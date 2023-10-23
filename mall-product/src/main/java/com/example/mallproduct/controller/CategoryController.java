209281f19cf9946b8c9c95ca3cfc2d95a0984a3e
DrinkingJuice <2042477660@qq.com>
2023-10-23 10:08:30 +0800

    firstStep project!(完成三级分类功能)

--- mall-product/src/main/java/com/example/mallproduct/controller/CategoryController.java
+++ mall-product/src/main/java/com/example/mallproduct/controller/CategoryController.java
@@ -35,9 +35,8 @@ public class CategoryController {
      * 查出所有分类以及子分类，以树形结构组装起来
      */
     @RequestMapping("/list/tree")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(){
         List<CategoryEntity> entities = categoryService.listWithTree();
-
         return R.ok().put("data", entities);
     }
 
@@ -48,7 +47,6 @@ public class CategoryController {
     @RequestMapping("/info/{catId}")
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
-
         return R.ok().put("category", category);
     }
 
@@ -58,7 +56,6 @@ public class CategoryController {
     @RequestMapping("/save")
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
-
         return R.ok();
     }
 
@@ -68,7 +65,6 @@ public class CategoryController {
     @RequestMapping("/update")
     public R update(@RequestBody CategoryEntity category){
 		categoryService.updateById(category);
-
         return R.ok();
     }
 
@@ -78,7 +74,6 @@ public class CategoryController {
     @RequestMapping("/delete")
     public R delete(@RequestBody Long[] catIds){
 		categoryService.removeByIds(Arrays.asList(catIds));
-
         return R.ok();
     }
 
--- mall-product/src/main/java/com/example/mallproduct/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/example/mallproduct/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.mallproduct.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,19 +33,17 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Override
     public List<CategoryEntity> listWithTree() {
-        //查出所有分类
+        //查出所有分类,以树形结构展示
         List<CategoryEntity> entities = baseMapper.selectList(null);
         //组装成所有分类的树形结构
         //1.找出父id为0的分类，也就是一级分类
         List<CategoryEntity> level1Menu = entities.stream()
                 .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
-                .map(meun -> {
-                    meun.setChildren(getChildren(meun, entities));
-                    return meun;
+                .map(menu -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
                 }) //递归找子菜单
-                .sorted((menu1, menu2) -> {
-                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
-                }) //排序
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))) //排序
                 .toList();
         return level1Menu;
     }
@@ -58,9 +57,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
             categoryEntity1.setChildren(getChildren(categoryEntity1, list));
             return categoryEntity1; //找子菜单
                 })
-                .sorted((menu1, menu2) -> {
-                    return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort()); //排序
-                }).toList();
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).toList();
         return children;
     }
 }
